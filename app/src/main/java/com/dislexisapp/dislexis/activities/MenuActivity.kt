8aab3d080d4467772bb7da1c9574d9c82aaa5525
Cristian Mundo <00086917@uca.edu.sv>
2019-06-24 09:41:41 -0600

    se agrego el app bar al menu

--- app/src/main/java/com/dislexisapp/dislexis/activities/MenuActivity.kt
+++ app/src/main/java/com/dislexisapp/dislexis/activities/MenuActivity.kt
@@ -1,9 +1,11 @@
 package com.dislexisapp.dislexis.activities
 
+import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity;
 import com.dislexisapp.dislexis.R
 import kotlinx.android.synthetic.main.content_menu.*
@@ -35,16 +37,34 @@ class MenuActivity : AppCompatActivity() {
         }
     }
 
-    /*override fun onCreateOptionsMenu(menu: Menu): Boolean {
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
         menuInflater.inflate(R.menu.menu_home, menu)
         return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
-            R.id.action_logout -> true
+            R.id.action_logout ->{
+                System.exit(0)
+                return true
+            }
             else -> super.onOptionsItemSelected(item)
         }
-    }*/
+    }
+/*
+    override fun onBackPressed() {
+        val builder = AlertDialog.Builder(application)
+        builder.setMessage("¿Seguro de cerrar sesion?")
+        builder.setCancelable(true)
+        builder.setNegativeButton("No") { dialog, _ ->
+            dialog.cancel()
+        }
+
+        builder.setPositiveButton("Exit") { _, _ ->
+            finish()
+        }
+        val alertDialog = builder.create()
+        alertDialog.show()
+    } TODO lo meti cuando presionara el de cerrar sesion pero se muere XD*/
 
 }
